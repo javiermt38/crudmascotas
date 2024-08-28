aa767b8abfac90540b89a2e382882f410c1e184b
Javier <andresrincon_34@hotmail.com>
2024-08-28 14:50:00 -0500

    nueva actualizacion

--- models/mascota.js
+++ models/mascota.js
@@ -1,10 +1,10 @@
-//const mongoose = require('mongoose');
-//const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-//const mascotaSchema = new Schema({
- //   nombre: String,
-   // descripcion: String
-//});
+const mascotaSchema = new Schema({
+    nombre: String,
+    descripcion: String
+});
 
 /* creacion del modelo */
 const Mascota = mongoose.model('Mascota', mascotaSchema);
